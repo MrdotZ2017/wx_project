0202d0d4113b170c344f768ed851452d9f74ee3f
MrdotZ2017 <zhangxiangying2011@163.com>
2018-05-17 15:40:04 +0800

    v1.0 20180517

--- test_postgresql.py
+++ test_postgresql.py
@@ -11,3 +11,4 @@ db = {
 conn = psycopg2.connect(dbname="mydb", user="dbuser", password="1234@qwer", host="rm-2zeg8s69g42akn9659o.pg.rds.aliyuncs.com", port="3432")
 cur = conn.cursor()
 print('连接成功.')
+
